5463a132f954dbc555a16c8b67079437a1bb535c
abem93 <aimejia4@gmail.com>
2024-08-13 23:26:46 -0500

    updated nav color gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {
         'cloudpano-blue': 'rgb(89, 193, 201)',
+        'background-blue': 'rgb(61,126,230)',
+        'middle-blue': 'rgb(18,134,251)'
       },
     },
     plugins: [],
