51f5fb602bed9a83b8dd6dec375649fbeb3b53f5
abem93 <aimejia4@gmail.com>
2024-08-16 05:35:31 -0500

    updated button backgrounds to gradient on hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'cloudpano-blue-green': 'rgb(89, 193, 201)',
+        'cloudpano-aqua': 'rgb(89, 193, 201)',
         'cloudpano-green': '#47ce90',
         'cloudpano-blue': '#4c93ec',
-        'middle-blue': 'rgb(18,134,251)'
+        'middle-blue': 'rgb(69,119,206)'
       },
     },
     plugins: [],
